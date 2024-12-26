63e7d23d45fc348359ab57155eb3fff3051f345b
Salvatore Impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-26 21:32:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      message: 'Template Vite + Vue'
+    }
+  },
+
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<template> 
+  <header>
+    <h1> 
+        <AppHeader />
+    </h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use 'assets/scss/main.scss' as *;
+
+header {
+    background-color: aquamarine;
+    text-align: center;
+}
+
+h1 {
+    color: lightcoral;
+}
+</style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<!-- All'interno del template ci va un solo figlio -->
+<template> 
+  <div>
+    <header>
+        <h1> 
+            {{ message }}
+        </h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
